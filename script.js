7479ed75f2bbc54d54c1074366f02e3ba9aaa073
Shafia <111538877+sfk30@users.noreply.github.com>
2023-10-24 12:53:05 +0100

    created displayBook function adding new books onto page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const bookCard = document.querySelector('.book-card')
+const addBook = document.getElementById('add')
+
+addBook.addEventListener('click', displayBook)
+
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,19 +13,26 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title,author,pages,read) {
   var newBook = new Book(title, author, pages, read)
   myLibrary.push(newBook)
 }
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
+// const deathlyHallows = new Book('Deathly Hallows', 'J K Rowling', '854', 'read')
 
 function displayBook() {
     // for (let i of myLibrary) {
     //     console.log(i)
     // }
     myLibrary.forEach((book) => {
-        console.log(book)
+        // bookCard.textContent = book
+        // console.log(book)
+        var output = ''
+        for (let key in book) {
+          output += key + ':' + book[key] + '\n'
+        }
+        bookCard.textContent += output  
+        myLibrary.shift()
     })
 }
-
-// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
-// const deathlyHallows = new Book('Deathly Hallows', 'J K Rowling', '854', 'read')
